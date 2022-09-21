82c17bef077225452c0281cc6e35c13a5396e403
GabrielBuckingham <81900622+GabrielBuckingham@users.noreply.github.com>
2022-09-21 12:45:19 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -71,12 +71,8 @@ Mouse mouse;
 
 class box {
 public:
-    double x = 1.0;
-    double y = 1.0;
-    double w = 1.0;
-    double h = 1.0;
-
-
+    double xpos = 1.0, ypos = 1.0;
+    double width = 1.0, height = 1.0;
 
     sf::Color color = sf::Color(255, 255, 255);
 
@@ -87,10 +83,10 @@ public:
     }
 
     void push(std::vector<sf::Vertex>& boxes) {
-        sf::Vertex a = sf::Vertex(sf::Vector2f(x    , y), color);
-        sf::Vertex b = sf::Vertex(sf::Vector2f(x + w, y), color);
-        sf::Vertex c = sf::Vertex(sf::Vector2f(x + w, y + h), color);
-        sf::Vertex d = sf::Vertex(sf::Vector2f(x    , y + h), color);
+        sf::Vertex a = sf::Vertex(sf::Vector2f(xpos, ypos), color);
+        sf::Vertex b = sf::Vertex(sf::Vector2f(xpos + width, ypos), color);
+        sf::Vertex c = sf::Vertex(sf::Vector2f(xpos + width, ypos + height), color);
+        sf::Vertex d = sf::Vertex(sf::Vector2f(xpos, ypos + height), color);
         index = boxes.size();
         boxes.push_back(a);
         boxes.push_back(b);
@@ -111,6 +107,10 @@ public:
     }
 
     void setDim(double x, double y, double w, double h, std::vector<sf::Vertex>& boxes) {
+        xpos = x;
+        ypos = y;
+        width = w;
+        height = h;
         boxes[index + 0].position = sf::Vector2f(x, y);
         boxes[index + 1].position = sf::Vector2f(x + w, y);
         boxes[index + 2].position = sf::Vector2f(x + w, y);
@@ -138,10 +138,8 @@ public:
 
 class rect {
 public:
-    double x = 1.0;
-    double y = 1.0;
-    double w = 1.0;
-    double h = 1.0;
+    double xpos = 1.0, ypos = 1.0;
+    double width = 1.0, height = 1.0;
 
     sf::Color color = sf::Color(255, 255, 255);
 
@@ -153,14 +151,10 @@ public:
 
     void push(std::vector<sf::Vertex>& rectangles) {
         index = rectangles.size();
-        /*rectangles.push_back(sf::Vertex(sf::Vector2f(x    , y), color));
-        rectangles.push_back(sf::Vertex(sf::Vector2f(x + w, y), color));
-        rectangles.push_back(sf::Vertex(sf::Vector2f(x + w, y + h), color));
-        rectangles.push_back(sf::Vertex(sf::Vector2f(x    , y + h), color));*/
-        rectangles.push_back(sf::Vertex(sf::Vector2f(x, y)));
-        rectangles.push_back(sf::Vertex(sf::Vector2f(x + w, y)));
-        rectangles.push_back(sf::Vertex(sf::Vector2f(x + w, y + h)));
-        rectangles.push_back(sf::Vertex(sf::Vector2f(x, y + h)));
+        rectangles.push_back(sf::Vertex(sf::Vector2f(xpos, ypos)));
+        rectangles.push_back(sf::Vertex(sf::Vector2f(xpos + width, ypos)));
+        rectangles.push_back(sf::Vertex(sf::Vector2f(xpos + width, ypos + height)));
+        rectangles.push_back(sf::Vertex(sf::Vector2f(xpos, ypos + height)));
     }
 
     void setColor(sf::Color newColor, std::vector<sf::Vertex>& rectangles) {
@@ -172,10 +166,14 @@ public:
     }
 
     void setDim(double x, double y, double w, double h, std::vector<sf::Vertex>& boxes) {
-        boxes[index + 0].position = sf::Vector2f(x    , y);
+        xpos = x;
+        ypos = y;
+        width = w;
+        height = h;
+        boxes[index + 0].position = sf::Vector2f(x, y);
         boxes[index + 1].position = sf::Vector2f(x + w, y);
         boxes[index + 2].position = sf::Vector2f(x + w, y + h);
-        boxes[index + 3].position = sf::Vector2f(x    , y + h);
+        boxes[index + 3].position = sf::Vector2f(x, y + h);
     }
 
     void setTextureDim(double x, double y, double w, double h, std::vector<sf::Vertex>& boxes) {
@@ -200,15 +198,6 @@ public:
     }
 };
 
-class widget {
-public:
-    double x = 0.0, y = 0.0;
-    double w = 0.0, h = 0.0;
-
-    widget() {}
-
-};
-
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(game.screenWidth, game.screenHeight), "Hello SFML", sf::Style::Close);
@@ -218,41 +207,27 @@ int main()
     std::vector<sf::Vertex> rectangles;
     sf::Texture rectTexture;
     if (!rectTexture.loadFromFile("images/sprites.png")) { std::cout << "nope" << std::endl; }
-    //rectTexture.loadFromFile("images/sprites.png");
-
-    //rect goofy_ahh_rect(rectangles);
-    //rect goofy_ahh_rect_(rectangles);
-    //rect goofy_ahh_rect__(rectangles);
-    //goofy_ahh_rect.setDim(300, 300, 40, 90, rectangles);
-    //goofy_ahh_rect.setColor(sf::Color(255, 255, 0), rectangles);
-    //goofy_ahh_rect_.setDim(350, 300, 40, 90, rectangles);
-    //goofy_ahh_rect_.setColor(sf::Color(0, 255, 255), rectangles);
-    //goofy_ahh_rect__.setDim(400, 300, 200, 200, rectangles);
-    ////goofy_ahh_rect__.setColor(sf::Color(255, 0, 255), rectangles);
-    //goofy_ahh_rect__.setTextureDim(0, 0, 50, 50, rectangles);
-    ////goofy_ahh_rect_.invisible(rectangles);
-
-
-    //box silly_test_box(boxes);
-    //box silly_test_box_2(boxes);
-    //silly_test_box.setColor(sf::Color(255, 0, 255), boxes);
-    //silly_test_box_2.setColor(sf::Color(0, 255, 255), boxes);
-    //silly_test_box.setDim(100, 100, 50, 50, boxes);
-    //silly_test_box_2.setDim(120, 120, 50, 50, boxes);
-
-    //silly_test_box.invisible(boxes);
-    //silly_test_box.visible(boxes);
 
     //rainbow
+    std::vector<rect> rect_instances;
+    for (int i = 0; i < 255; i++) {
+        rect rect(rectangles);
+        //rect.setColor(sf::Color(255 - i, 0, i), rectangles);
+        rect.setDim(i * 5.843 + 40, i * 2 + 40, 30, 30, rectangles);
+        rect_instances.push_back(rect);
+    }
+    rect& rect = rect_instances[150];
+    rect.setDim(rect.xpos, rect.ypos - 50, 30, 30, rectangles);
+
     std::vector<box> box_instances;
     for (int i = 0; i < 255; i++) {
         box box(boxes);
         box.setColor(sf::Color(255 - i, 0, i), boxes);
-        box.setDim(i*5.843 + 40, i*3.098 + 40, 30, 30, boxes);
+        box.setDim(i * 5.843 + 40, i * 2 + 200, 30, 30, boxes);
         box_instances.push_back(box);
     }
-
-    //box_instances[150].setDim(box_instances[150].x, 100, 60, 60, boxes); instance doesnt actually know its own dimension, should fix
+    box& box = box_instances[150];
+    box.setDim(box.xpos, box.ypos - 50, 30, 30, boxes);
 
     while (window.isOpen()) {
         mouse.setMouseProperties(sf::Mouse::getPosition(window));
@@ -264,12 +239,9 @@ int main()
             }
         }
 
-        //goofy_ahh_rect_.setDim(350, 300, 40, mouse.y - 300, rectangles);
-        //goofy_ahh_rect__.setDim(400, 300, mouse.x - 400, 200, rectangles);
-
         window.clear(sf::Color(0, 0, 0));
-        window.draw(&boxes[0], boxes.size(), sf::Lines);
-        //window.draw(&rectangles[0], rectangles.size(), sf::Quads, &rectTexture);
+        if (boxes.size() > 0) { window.draw(&boxes[0], boxes.size(), sf::Lines); }
+        if (rectangles.size() > 0) { window.draw(&rectangles[0], rectangles.size(), sf::Quads, &rectTexture); }
         window.display();
     }
     return 0;
